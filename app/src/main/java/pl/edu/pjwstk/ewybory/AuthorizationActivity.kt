33c13f49cf242b510ba554ce74a54a54da51c810
CampellRadomir <karol.michalak96@gmail.com>
2022-03-25 18:43:07 +0100

    material design, dodanie motywu jasnego i ciemnego

--- app/src/main/java/pl/edu/pjwstk/ewybory/AuthorizationActivity.kt
+++ app/src/main/java/pl/edu/pjwstk/ewybory/AuthorizationActivity.kt
@@ -2,8 +2,11 @@ package pl.edu.pjwstk.ewybory
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import pl.edu.pjwstk.ewybory.databinding.ActivityAuthorizationBinding
 
 class AuthorizationActivity : AppCompatActivity() {
@@ -23,5 +26,25 @@ class AuthorizationActivity : AppCompatActivity() {
         val edoAppAuthorizationIntent = Intent(this, PassportAuthActivity::class.java)
         edoAppAuthorizationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         startActivity(edoAppAuthorizationIntent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_light_theme -> changeTheme(AppCompatDelegate.MODE_NIGHT_NO)
+            R.id.menu_dark_theme -> changeTheme(AppCompatDelegate.MODE_NIGHT_YES)
+            R.id.menu_system_theme -> changeTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun changeTheme(themeMode: Int): Boolean {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        delegate.applyDayNight()
+        return true
     }
 }
